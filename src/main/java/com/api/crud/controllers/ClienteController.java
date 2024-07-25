66009e334eab08e118930860cfcc1a6a50a9df5d
willas <nascimentowillas5@gmail.com>
2024-07-25 16:23:37 -0300

    update funcionando o delete não funciona no insomnia

--- src/main/java/com/api/crud/controllers/ClienteController.java
+++ src/main/java/com/api/crud/controllers/ClienteController.java
@@ -1,8 +1,10 @@
 package com.api.crud.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +39,29 @@ public class ClienteController {
         }
         return "Cliente não encontrado.";
     }
+
+    @PutMapping("/{id}")
+    public String updateCliente(@PathVariable UUID id, @RequestBody cliente updatedCliente) {
+        for (cliente cliente : cliente.clientes) {
+            if (cliente.getId().equals(id)) {
+                cliente.setNome(updatedCliente.getNome());
+                return "Cliente atualizado com sucesso!";
+            }
+        }
+        return "Cliente não encontrado";
+    }
+
+    /* não esta funcionado no insonia */
+    @DeleteMapping("/{id}")
+    public String deletarCliente(@PathVariable UUID Id) {
+        for (cliente cliente : cliente.clientes) {
+            if (cliente.getId().equals(Id)) {
+                cliente.clientes.remove(cliente);
+                return "Cliente deletado com sucesso. ";
+            }
+        }
+        return "Cliente não encontrado.";
+    }
+    
 
 }
